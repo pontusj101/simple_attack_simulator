1fac843b8392779c7083b9deedb311a7e56e0690
pontusj101 <9636072+pontusj101@users.noreply.github.com>
2024-08-04 10:22:16 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simple_attack_simulator',
+    version='0.1.0',
+    description='Creates random attack graphs, simulates attacks, and produces logs.',
+    author='Pontus Johnson',
+    author_email='your.email@example.com',
+    url='https://github.com/pontusj101/simple_attack_simulator',
+    license='Apache-2.0',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        # List your project dependencies here
+        # e.g., 'numpy', 'pandas',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
